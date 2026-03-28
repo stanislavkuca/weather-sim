cccbd0959308507a6be6d86fb37ffa1cfc401951
Stanislav Kuca <kucastanik@gmail.com>
2026-03-28 15:52:23 +0100

    chore: add weather genaration to its own file

--- WeatherSim/MainWindow.xaml.cs
+++ WeatherSim/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Reflection.Emit;
 using System.Text;
 using System.Windows;
 using System.Windows.Automation.Text;
@@ -11,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WeatherSim.Models;
+using WeatherSim.Services;
 
 namespace WeatherSim
 {
@@ -22,12 +24,11 @@ namespace WeatherSim
         public MainWindow()
         {
             InitializeComponent();
-
             weatherItemCollection.ItemsSource = days;
         }
 
         ObservableCollection<Day> days = new ObservableCollection<Day>();
-        Random random = new Random();
+        WeatherGenerator generator = new WeatherGenerator();
 
         private void SimulateButton(object sender, RoutedEventArgs e)
         {
@@ -39,10 +40,10 @@ namespace WeatherSim
             for (int i = 0; i < simDayCount; i++)
             {
                 var date = DateTime.Today.AddDays(i);
-                var season = GetSeason(date);
+                var season = generator.GetSeason(date);
 
-                var temperature = GenerateTemperature(season);
-                var (weather, icon) = PickWeather(season);
+                var temperature = generator.GenerateTemperature(season);
+                var (weather, icon) = generator.PickWeather(season);
 
                 days.Add(new Day
                 {
@@ -52,66 +53,6 @@ namespace WeatherSim
                     Weather = weather
                 });
             }
-        }
-
-        private string GetSeason(DateTime date)
-        {
-            int month = date.Month;
-
-            return month switch
-            {
-                12 or 1 or 2 => "winter",
-                3 or 4 or 5 => "spring",
-                6 or 7 or 8 => "summer",
-                _ => "autumn"
-            };
-        }
-
-        private int GenerateTemperature(string season)
-        {
-            return season switch
-            {
-                "winter" => random.Next(-10, 6),
-                "spring" => random.Next(5, 18),
-                "summer" => random.Next(18, 36),
-                "autumn" => random.Next(5, 15),
-                _ => 0
-            };
-        }
-
-        private (string weather, string iconPath) PickWeather(string season)
-        {
-            var cloudy = ("Cloudy", "/Assets/Icons/weather_cloudy.png");
-            var snowy = ("Snowy", "/Assets/Icons/weather_snowy.png");
-            var sunny = ("Sunny", "/Assets/Icons/weather_sunny.png");
-            var fog = ("Fog", "/Assets/Icons/weather_fog.png");
-            var storm = ("Storm", "/Assets/Icons/weather_storm.png");
-            var rain = ("Rain", "/Assets/Icons/weather_rain.png");
-
-            int roll = random.Next(100);
-
-            return season switch
-            {
-                "winter" => roll switch
-                {
-                    < 30 => (cloudy),
-                    < 70 => (snowy),
-                    < 75 => (fog),
-                    < 85 => (rain),
-                    _ => (sunny)
-                },
-
-                "summer" => roll switch
-                {
-                    < 5 => (fog),
-                    < 50 => (sunny),
-                    < 60 => (storm),
-                    < 90 => (cloudy),
-                    _ => (rain),
-                },
-
-                _ => (cloudy)
-            };
         }
     }
 }
---
+++ WeatherSim/Services/WeatherGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherSim.Services
+{
+    internal class WeatherGenerator
+    {
+        Random random = new Random();
+
+        public string GetSeason(DateTime date)
+        {
+            int month = date.Month;
+
+            return month switch
+            {
+                12 or 1 or 2 => "winter",
+                3 or 4 or 5 => "spring",
+                6 or 7 or 8 => "summer",
+                _ => "autumn"
+            };
+        }
+
+        public int GenerateTemperature(string season)
+        {
+            return season switch
+            {
+                "winter" => random.Next(-10, 6),
+                "spring" => random.Next(5, 18),
+                "summer" => random.Next(18, 36),
+                "autumn" => random.Next(5, 15),
+                _ => 0
+            };
+        }
+
+        public (string weather, string iconPath) PickWeather(string season)
+        {
+            var cloudy = ("Cloudy", "/Assets/Icons/weather_cloudy.png");
+            var snowy = ("Snowy", "/Assets/Icons/weather_snowy.png");
+            var sunny = ("Sunny", "/Assets/Icons/weather_sunny.png");
+            var fog = ("Fog", "/Assets/Icons/weather_fog.png");
+            var storm = ("Storm", "/Assets/Icons/weather_storm.png");
+            var rain = ("Rain", "/Assets/Icons/weather_rain.png");
+
+            int roll = random.Next(100);
+
+            return season switch
+            {
+                "winter" => roll switch
+                {
+                    < 30 => (cloudy),
+                    < 70 => (snowy),
+                    < 75 => (fog),
+                    < 85 => (rain),
+                    _ => (sunny)
+                },
+
+                "summer" => roll switch
+                {
+                    < 5 => (fog),
+                    < 50 => (sunny),
+                    < 60 => (storm),
+                    < 90 => (cloudy),
+                    _ => (rain),
+                },
+
+                _ => (cloudy)
+            };
+        }
+    }
+}
